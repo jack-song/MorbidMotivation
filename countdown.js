b7b4c843fa839ed230d498dffa473736189e569f
Jack <songjiapei@gmail.com>
2014-11-21 23:33:38 -0500

    - added in setting and retrieving data - adjusted styles

--- countdown.js
+++ countdown.js
@@ -1,35 +1,63 @@
-var yearsCount = document.getElementById("years"),
+var STORAGE_BIRTH_NAME = "birthDateNum",
+	STORAGE_LIFE_NAME = "lifeNum",
+	yearsCount = document.getElementById("years"),
 	monthsCount = document.getElementById("months"),
 	daysCount = document.getElementById("days"),
 	hoursCount = document.getElementById("hours"),
 	minutesCount = document.getElementById("minutes"),
 	secondsCount = document.getElementById("seconds"),
+	birthDateInput = document.getElementById("birthDate"),
+	lifeInput = document.getElementById("life"),
 	setButton = document.getElementById("set"),
 	settingsButton = document.getElementById("settings"),
 	settingsSection = document.getElementById("settingsSection"),
 	displaySection = document.getElementById("displaySection");
 
-var birthDate = new Date(2000, 1, 1),
-	lifeExpectancy = 75,
-	deathDate = birthDate.setFullYear(birthDate.getFullYear() + lifeExpectancy);
+var	deathDate = new Date(0);
 
-//run once right away
-count();
+load();
 window.setInterval(count, 1000);
 
 setButton.onclick = function(){
-	displaySection.style.display = 'block';
-	settingsSection.style.display = 'none';
+	//get birthday and life expectancy from form
+	var birthDateString = birthDateInput.value,
+		lifeExpectancy = parseInt(lifeInput.value),
+		deathDateNum = 0;
+
+	//both fields must be filled
+	if(!birthDate || !lifeExpectancy)
+		return;
+
+	var store = {}
+	store[STORAGE_BIRTH_NAME] = birthDateString;
+	store[STORAGE_LIFE_NAME] = lifeExpectancy;
+
+	chrome.storage.sync.set({data: store}, doneSave);
 }
 
 settingsButton.onclick = function(){
+	var birthDate,
+		lifeExpectancy;
+
+	//try to load saved data into inputs
+	chrome.storage.sync.get("data", function(result) {
+		var data = result.data;
+		birthDate = data[STORAGE_BIRTH_NAME];
+		lifeExpectancy = data[STORAGE_LIFE_NAME];
+
+		if(birthDate)
+			birthDateInput.value = birthDate;
+		if(lifeExpectancy)
+			lifeInput.value = lifeExpectancy;
+	});
+
 	displaySection.style.display = 'none';
 	settingsSection.style.display = 'block';
 }
 
 function count() {
-	var currentDate = new Date().getTime(),
-    	elapsedMillis = deathDate - currentDate,
+	var currentDate = new Date(),
+    	elapsedMillis = deathDate.getTime() - currentDate.getTime(),
     	baseDate = new Date(0),
     	elapsedDate = new Date(elapsedMillis);
 
@@ -46,4 +74,51 @@ function count() {
     hoursCount.innerHTML = hours;
     minutesCount.innerHTML = minutes;
     secondsCount.innerHTML = seconds;
+}
+
+function load() {
+	var birthDate,
+		lifeExpectancy,
+		deathDateNum = 0;
+
+	//loads birthday and life expectancy
+	chrome.storage.sync.get("data", function(result) {
+		var data = result.data,
+			failed = false;
+
+		if(!data){
+			failed = true;
+		}
+		else {
+			birthDate = new Date(data[STORAGE_BIRTH_NAME]);
+			lifeExpectancy = data[STORAGE_LIFE_NAME];
+		}
+
+		if(!birthDate || !lifeExpectancy){
+			failed = true;
+		}
+		else{
+			//convert date of birth to death by adding life expectency
+			birthDate.setFullYear(birthDate.getFullYear() + lifeExpectancy);
+			deathDate = new Date(birthDate.getTime());
+
+			if(isNaN(deathDate.getTime()))
+				failed = true;
+		}
+
+		if(failed){
+			displaySection.style.display = 'none';
+			settingsSection.style.display = 'block';
+		}
+		else{
+			count();
+		}
+	});
+}
+
+function doneSave() {
+	load();
+	count();
+	displaySection.style.display = 'block';
+	settingsSection.style.display = 'none';
 }
